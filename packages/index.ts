dd28901630fc32386485f6bc3013561b2b2af6bf
Diaz <zenglingyu@legendapl.com.cn>
2020-06-17 17:55:50 +0800

    update .d.ts

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-06-15 16:00:35
  * @LastEditors: 曾令宇
- * @LastEditTime: 2020-06-17 17:23:59
+ * @LastEditTime: 2020-06-17 17:55:05
  * @FilePath: \element-dynamic-form-item\packages\index.ts
  */
 import { DynamicFormItem } from './dynamic-form-item'
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,9 @@
 /*
  * @Date: 2020-06-17 15:22:13
  * @LastEditors: 曾令宇
- * @LastEditTime: 2020-06-17 15:35:36
+ * @LastEditTime: 2020-06-17 17:54:53
  * @FilePath: \element-dynamic-form-item\types\index.d.ts
  */
-import * as ElDynamicFormItem from './dynamic-form-item';
+import ElDynamicFormItem from '../packages';
 
 export default ElDynamicFormItem;
