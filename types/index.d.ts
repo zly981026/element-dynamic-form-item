573f6425a5e6b47d4930e85e40d8b1e3c04d90c5
Diaz <diaz_zeng@outlook.com>
2020-06-17 15:53:08 +0800

    fix bug

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Date: 2020-06-17 15:22:13
- * @LastEditors: 曾令宇
- * @LastEditTime: 2020-06-17 15:35:36
- * @FilePath: \element-dynamic-form-item\types\index.d.ts
- */
-import * as ElDynamicFormItem from './dynamic-form-item';
-
-export default ElDynamicFormItem;
