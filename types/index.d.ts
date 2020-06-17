19353162b0a5834c9c7a7171950d32b5aa6dadff
Diaz <diaz_zeng@outlook.com>
2020-06-17 15:58:31 +0800

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2020-06-17 15:22:13
+ * @LastEditors: 曾令宇
+ * @LastEditTime: 2020-06-17 15:35:36
+ * @FilePath: \element-dynamic-form-item\types\index.d.ts
+ */
+import * as ElDynamicFormItem from './dynamic-form-item';
+
+export default ElDynamicFormItem;
